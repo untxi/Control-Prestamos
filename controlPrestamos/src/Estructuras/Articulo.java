cc61a1f296a25238f4fb031fcab63920c48adc30
asoto134 <sotoadrian4@gmail.com>
2014-08-24 16:59:10 -0600

    Revert "Agregar a la clase Artículo:"
    This reverts commit f0a7929c0bdc8674f1480e0713462aae2d692087.

--- controlPrestamos/src/Estructuras/Articulo.java
+++ controlPrestamos/src/Estructuras/Articulo.java
@@ -1,19 +1,14 @@
 package Estructuras;
 
-import java.util.ArrayList;
-
 public class Articulo {
 	private String nombre;
 	private int calificacion;
 	private String imagen;
-	private int cantidadPrestamos;
-	private ArrayList<Integer> fechaPrestamo = new ArrayList<Integer>();
-	int meses = 12;
 	
 	public Articulo(String nombre, int calificacion, String imagen) {
 		this.nombre = nombre;
 		this.calificacion = calificacion;
-		this.imagen = imagen;	
+		this.imagen = imagen;
 	}
 
 	public String getNombre() {
@@ -38,21 +33,5 @@ public class Articulo {
 
 	public void setImagen(String imagen) {
 		this.imagen = imagen;
-	}
-
-	public ArrayList<Integer> getFechaPrestamo() {
-		return fechaPrestamo;
-	}
-
-	public void setFechaPrestamo(ArrayList<Integer> fechaPrestamo) {
-		this.fechaPrestamo = fechaPrestamo;
-	}
-
-	public int getCantidadPrestamos() {
-		return cantidadPrestamos;
-	}
-
-	public void setCantidadPrestamos(int cantidadPrestamos) {
-		this.cantidadPrestamos = cantidadPrestamos;
 	}
 }
